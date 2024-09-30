7e933aa272f29ce5c82d8d87cd4011942a9d7f94
Paloftzer <phille04@outlook.com>
2024-09-30 13:37:30 +0200

    Created a new object of person with dummy data

---
+++ Lesson-13/ReadingandWritingJsonFiles/Person.cs
@@ -0,0 +1,18 @@
+namespace ReadingandWritingJsonFiles;
+
+public class Person
+{
+    public DateTime CreatedAt { get; set; }
+    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public int Age { get; set; }
+
+    public override string ToString()
+    {
+        return $"Created at: {CreatedAt}, Name: {FirstName}, {LastName}, Email: {Email}, Phone Number: {PhoneNumber}, Age: {Age}";
+    }
+}
--- Lesson-13/ReadingandWritingJsonFiles/Program.cs
+++ Lesson-13/ReadingandWritingJsonFiles/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main()
     {
-        
+        Person person = new();
+        person.CreatedAt = DateTime.Now;
+        person.FirstName = "John";
+        person.LastName = "Doe";
+        person.Email = "doe@example.com";
+        person.PhoneNumber = "555-555812";
+        person.Age = 27;
     }
 }

1883fdb0e4a52eaca80bdef09c1d43da9cb6c814
Paloftzer <phille04@outlook.com>
2024-09-30 11:55:14 +0200

    Added capability to read and write text to and from files

--- Lesson-13/ReadWriteFiles/Program.cs
+++ Lesson-13/ReadWriteFiles/Program.cs
@@ -4,7 +4,13 @@ class Program
 {
     static void Main()
     {
-        var path = Environment.CurrentDirectory + "/data/Log.txt";
+        var path = Environment.CurrentDirectory + "/data/log.txt";
         Console.WriteLine(path);
+
+        var message = $"info added - {DateTime.Now.ToShortDateString()}";
+
+        File.WriteAllText(path, message);
+
+        Console.WriteLine(File.ReadAllText(path));
     }
 }
